8983d64e4037808c0feac3aedd5720c4d85e0e2a
Mandar Sanjay Yadav <103592895+MandarYadav24@users.noreply.github.com>
2026-08-18 05:26:09 +0530

    Add files via upload

---
+++ select.go
@@ -0,0 +1,47 @@
+package main
+import (
+  "fmt"
+  "time"
+)
+
+func main() {
+  ch1 := make(chan string)
+  ch2 := make(chan string)
+
+  go func() {
+    time.Sleep(1 * time.Second)
+    ch1 <- "one"
+  }()
+
+  go func() {
+    time.Sleep(2 * time.Second)
+    ch2 <- "two"
+  }()
+
+  for range 2 {
+    select {
+    case msg1 := <-ch1:
+      fmt.Println("received:",msg1)
+    case msg2 := <-ch2:
+      fmt.Println("received:",msg2)
+   }
+  } 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
