bb9c287786ad276642f7ebebd95a0f55339a5752
Mandar Sanjay Yadav <103592895+MandarYadav24@users.noreply.github.com>
2026-08-18 05:50:50 +0530

    Add files via upload

---
+++ nonBlockingChannels.go
@@ -0,0 +1,53 @@
+package main
+import (
+  "fmt"
+)
+
+func main() {
+  messages := make(chan string)
+  signals := make(chan bool)
+
+  select {
+    case msg := <- messages :
+      fmt.Println("Recevied msg:",msg)
+    default :
+      fmt.Println("No message received")
+  }
+
+  msg := "Hi"
+
+  select {
+    case messages <- msg:
+      fmt.Println("Sent message:",msg)
+    default :
+      fmt.Println("No message sent")
+  }
+
+  select {
+    case msg := <- messages:
+      fmt.Println("Received msg:",msg)
+    case sig := <-signals:
+      fmt.Println("Received signal:",sig)
+    default :
+      fmt.Println("No activity...")
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
